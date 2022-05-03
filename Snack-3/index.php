523da673eb30d37eb4ee1b81abae89bd9e0b021b
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-05-03 16:27:43 +0200

    Snack3 done

--- Snack-3/index.php
+++ Snack-3/index.php
@@ -2,4 +2,18 @@
 // Snack 3
 // Creare un array con 15 numeri casuali, tenendo conto 
 // che l’array non dovrà contenere lo stesso numero più di una volta
-/>
+
+// Data 
+$randomNumbers = [];
+
+while(count($randomNumbers) < 15) {
+    $randomNum = rand(1, 100);
+
+    if (!in_array(($randomNum), $randomNumbers)) {
+        $randomNumbers[] = $randomNum;
+    }
+}
+echo('<pre>');
+var_dump($randomNumbers);
+echo('</pre>');
+?>
